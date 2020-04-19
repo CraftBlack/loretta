0dccc9e28ea5d85b2a598d6248197d22c7e2ef33
Jorge Guerra <jorgeguerra1098@gmail.com>
2020-04-18 22:13:09 -0600

    start script added, and export correction

---
+++ events.js
@@ -0,0 +1,25 @@
+const events = [
+    {
+        hours: 0,
+        minutes: 0,
+        message: '00:00 Nuevo día del servidor. Lanza tu deseo.', 
+    },
+    {
+        hours: 5,
+        minutes: 0,
+        message: `
+            1. Reset del servidor. Ahora están nuevamente disponibles las siguientes recompensas:
+                * Entrenamiento Diario
+                * Calabozo de Legión
+                * Ayuda de Legión
+                * Recompensas de Legión.
+        ` 
+    },
+    {
+        hours: 9,
+        minutes: 0,
+        message: '09:00 Recuerda realizar el Tesoro del Patriarca Avanzado.', 
+    }
+];
+
+module.exports = events;
