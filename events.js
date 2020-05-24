1e2fa17324a9a872bcce7486782169bd4eb9325d
Jorge Guerra <jorgeguerra1098@gmail.com>
2020-05-24 12:17:28 -0600

    Set events

--- events.js
+++ events.js
@@ -1,18 +1,84 @@
 const events = [
     {
-        hours: 0,
+        hours: 23,
         minutes: 0,
-        message: '00:00 Erste Veranstaltung', 
+        message: `Nuevo día del servidor. Lanza tu deseo`,
     },
     {
-        hours: 1,
+        hours: 4,
         minutes: 0,
-        message: '00:00 Zweiter Veranstaltung', 
+        message: `
+            1. Reset del servidor. Ahora están nuevamente disponibles las siguientes recompensas:
+                * Entrenamiento Diario
+                * Calabozo de Legión
+                * Ayuda de Legión
+                * Recompensas de Legión.
+        `, 
     },
     {
-        hours: 2,
+        hours: 8,
         minutes: 0,
-        message: '00:00 Dritter Veranstaltung', 
+        message: `Recuerda realizar el Tesoro del Patriarca Avanzado.`, 
+    },
+    {
+        hours: 12,
+        minutes: 0,
+        message: `Inicio del Jefe de legión.`, 
+    },
+    {
+        hours: 12,
+        minutes: 0,
+        message: `Inicia Duelo Galáctico.`, 
+    },
+    {
+        hours: 12,
+        minutes: 0,
+        message: `Inicia Ciclo Astral`, 
+    },
+    {
+        hours: 14,
+        minutes: 0,
+        message: `El Duelo Galáctico ha terminado.`
+    },
+    {
+        hours: 15,
+        minutes: 45,
+        message: `El jefe de Legión está a punto de terminar. Restan 15 minutos.`
+    },
+    {
+        hours: 14,
+        minutes: 00,
+        message: `Jefe de legión ha terminado.`
+    },
+    {
+        hours: 17,
+        minutes: 0,
+        message:  `
+            1. Recuerda participar en las actividades de legión:
+                * Calabozo de Legión
+                * Ayuda de Legión
+                * Recompensas de Legión
+        `
+    },
+    {
+        hours: 18,
+        minutes: 0,
+        message: `Inicia Duelo Galáctico`
+    },
+    {
+        hours: 18,
+        minutes: 30,
+        message: `Restan 30 minutos de duelo galáctico.`
+    },
+    {
+        hours: 22,
+        minutes: 0,
+        message: `El Duelo Galáctico ha terminado.`
+    },
+    {
+        hours: 22,
+        minutes: 30,
+        message: `El Ciclo Astral está a punto de terminar. Restan 30 minutos.`
     }
 ];
 
